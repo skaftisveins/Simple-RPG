90307b8fa38622b43b48abc8c79be1e8a0522486
skaftisveins <ss1.h18@nemandi.ntv.is>
2018-10-24 15:39:57 +0000

    SuperAdventure_version_1.0

---
+++ Engine/HealingPotion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class HealingPotion : Item // Inheritance, child class or derived class from Item class
+    {
+        public int AmountToHeal { get; set; }
+
+        public HealingPotion(int id, string name, string namePlural,
+            int amountToHeal) : base(id, name, namePlural) // take values from the parameters in the class
+        {
+            AmountToHeal = amountToHeal;
+        }
+    }
+}
---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class RandomNumberGenerator
+    {
+        private static Random _genarator = new Random();
+
+        public static int NumberBetween(int minimumValue, int maximumValue)
+        {
+            return _genarator.Next(minimumValue, maximumValue + 1);
+        }
+    }
+}
